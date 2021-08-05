cc42a1d8817cd72c2bbb51d5fafb1367453e07b3
LeonarDev <leonardo.majevski@gmail.com>
2021-08-05 20:19:31 -0300

    first commit

---
+++ Course/Course/Entities/Order.cs
@@ -0,0 +1,17 @@
+﻿using Course.Entities.Enums;
+using System;
+
+namespace Course.Entities
+{
+    class Order
+    {
+        public int Id { get; set; }
+        public DateTime Moment { get; set; }
+        public OrderStatus Status { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Id}, {Moment}, {Status}";
+        }
+    }
+}

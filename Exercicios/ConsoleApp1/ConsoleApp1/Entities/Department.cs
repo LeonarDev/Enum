7a6b660c62e3cb090cf5fbb38c50f5836851e219
LeonarDev <leonardo.majevski@gmail.com>
2021-08-06 16:30:52 -0300

    implementa as entidades a partir do UML

---
+++ Exercicios/ConsoleApp1/ConsoleApp1/Entities/Department.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsoleApp1.Entities
+{
+    class Department
+    {
+        public string Name { get; set; }
+
+        public Department()
+        {
+        }
+
+        public Department(string name)
+        {
+            Name = name;
+        }
+    }
+}
